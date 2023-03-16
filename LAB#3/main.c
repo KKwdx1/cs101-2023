0366c3463b67e35f1a19e9ff632af1e4bf3a50b9
KKwdx1 <113514776+KKwdx1@users.noreply.github.com>
2023-03-16 15:08:25 +0800

    Add files via upload

---
+++ LAB#3/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+typedef struct person {
+    int id;
+    char first_name[32];
+    char last_name[32];
+}person_t;
+
+int main(){
+   FILE* wfp;
+   wfp = fopen("person.bin","w+");
+   person_t p[5] = { {1,"IU","Lee"}, 
+                     {2,"IUU","Lee"},
+                     {3,"IUUU","Lee"},
+                     {4,"IUUUU","Lee"},                     
+                     {5,"IUUUUU","Lee"},
+};
+for (int i=0;i<5;i++) {
+    fwrite(p+i, sizeof(person_t), 1, wfp);
+    }
+    
+    person_t tmp;
+    int i=0;
+    while (fread(&tmp, sizeof(person_t), 1, wfp)) {
+        printf("[%d] %d: %s %s\n", i,
+                                    tmp.id,
+                                    tmp.first_name,
+                                    tmp.last_name);
+        i++;                            
+    }
+    fclose(wfp);
+return 0;
+}
